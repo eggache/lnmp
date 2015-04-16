40c4ee9dceeebf0d6ef6fef5d3a53ae3f56211d4
trylen <trylen@trylendeMacBook-Pro.local>
2015-04-16 19:56:35 +0800

    update

--- config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii',
-    'username' => 'root',
-    'password' => 'aszmq123',
-    'charset' => 'utf8',
-];
